e7f0c50f1044b792e48c4daba301304183832dd4
djordje <dpetrovic@wynd.eu>
2019-03-27 12:22:02 +0100

    google log in/log out

--- src/components/nav/MainNavLinks.js
+++ src/components/nav/MainNavLinks.js
@@ -3,35 +3,49 @@ import { NavLink } from 'react-router-dom';
 
 class MainNavLinks extends Component {
 
-  logout() {    
-    window.FB.getLoginStatus(function(response) {
-      if (response.status === 'connected') {
-        console.log('connected')
-        window.FB.logout(function(response) {
-            console.log(response)
-            localStorage.removeItem('login');
-            console.log('logged out')
-          });  
-      } else {
-        console.log('not connected')
-      }
-    });
-  }
+	logout() {
+		if (localStorage.login) {
+			let localStorageParse = JSON.parse(localStorage.login);
+			if (localStorageParse.method === 'facebook') {
+				window.FB.getLoginStatus(function (response) {
+					if (response.status === 'connected') {
+						console.log('connected')
+						window.FB.logout(function (response) {
+							console.log(response)
+							localStorage.removeItem('login');
+							console.log('logged out')
+						});
+					} else {
+						console.log('not connected')
+					}
+				});
+			} else if (localStorageParse.method === 'google') {
+				window.googleAuthObject.signOut()
+					.then(() => {
+						console.log(localStorage)
+						localStorage.removeItem('login');
+						console.log(localStorage)
+					})
+			}
+		} else {
+			console.log('not connected at all')
+		}
+	}
 
-  render() {
-    return (
-      <ul className="list white flex">
-        <li className="mr4">
-          <NavLink exact className="white link pt2 ttu" activeClassName="bt bw2 b--white" to="/">songs</NavLink>
-        </li>
-        <li className="mr4">
-          <NavLink className="white link pt2 ttu" activeClassName="bt bw2 b--white" to="/account">account</NavLink>
-        </li>
-        <li className="mr4">
-          <span className="pt2 ttu" onClick={this.logout}>log out</span>
-        </li>
-      </ul>);
-  }
+	render() {
+		return (
+			<ul className="list white flex">
+				<li className="mr4">
+					<NavLink exact className="white link pt2 ttu" activeClassName="bt bw2 b--white" to="/">songs</NavLink>
+				</li>
+				<li className="mr4">
+					<NavLink className="white link pt2 ttu" activeClassName="bt bw2 b--white" to="/account">account</NavLink>
+				</li>
+				<li className="mr4">
+					<span className="pt2 ttu" onClick={this.logout}>log out</span>
+				</li>
+			</ul>);
+	}
 }
 
 export default MainNavLinks;
--- src/pages/public/Login.js
+++ src/pages/public/Login.js
@@ -3,34 +3,50 @@ import fulllogo from '../../assets/img/rchain-fulllogo.svg';
 
 class Login extends Component {
 
-  fbLogin() {
-    window.FB.login(function(response){
-      console.log(response)
-      localStorage.setItem('login', JSON.stringify(
-        {
-          id: response.id,
-          method: 'facebook',
-        }
-      ))
-    }, {scope: 'email'});
-  }
+	fbLogin() {
+		window.FB.login(function (response) {
+			console.log(response)
+			localStorage.setItem('login', JSON.stringify(
+				{
+					id: response.id,
+					method: 'facebook',
+				}
+			))
+		}, { scope: 'email' });
+	}
 
-  render() {   
-    return (
-      <div className="w-100 vh-100 flex">
-        <div className="w-50 bg-blue">
-          <div className="w-100 pt2 pl2"><img src={fulllogo} /></div>
-          <div className="ph5 pt5">
-            <p className="f1 b white lh-title">Take control<br /> of your<br /> music.</p>
-          </div>
-        </div>
-        <div className="w-50 ph5 pt7 bg-black">
-          <a className="facebook-blue db white pv3 ph3 mb4 mw6 br2 f5 tc center" onClick={this.fbLogin}>Login / Register with Facebook</a>
-          <a className="google-red db white pv3 ph3 mw6 br2 f5 tc center">Login / Register with Google</a>
-        </div>
-      </div>
-    );
-  }
+	googleLogin() {
+		window.googleAuthObject.signIn(
+			{
+				scope: 'profile email'
+			}
+		).then((response) => {
+			localStorage.setItem('login', JSON.stringify(
+				{
+					id: response.El,
+					method: 'google',
+				}
+			))
+			console.log(response);
+		})
+	}
+
+	render() {
+		return (
+			<div className="w-100 vh-100 flex">
+				<div className="w-50 bg-blue">
+					<div className="w-100 pt2 pl2"><img src={fulllogo} /></div>
+					<div className="ph5 pt5">
+						<p className="f1 b white lh-title">Take control<br /> of your<br /> music.</p>
+					</div>
+				</div>
+				<div className="w-50 ph5 pt7 bg-black">
+					<a className="facebook-blue db white pv3 ph3 mb4 mw6 br2 f5 tc center" onClick={this.fbLogin}>Login / Register with Facebook</a>
+					<a className="google-red db white pv3 ph3 mw6 br2 f5 tc center" onClick={this.googleLogin}>Login / Register with Google</a>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Login;
