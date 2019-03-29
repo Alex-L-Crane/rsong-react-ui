707160bb6a03a80399b8b0a78ed82b28eea01c8f
Djordje Petrovic <djordje.petrovic@cramick-it.com>
2019-03-29 10:08:17 +0100

    names fix

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux';
 
 import { addKyc } from './addKycReducer';
-import { addSong } from './addSongsReducer';
+import { addSong } from './addSongReducer';
 import { countries } from './countriesReducer';
 import { genres } from './genresReducer';
 import { kyc } from './fetchKycReducer';
 import { song } from './fetchSongReducer';
-import { songs } from './songReducer';
+import { songs } from './songsReducer';
 import { updateKyc } from './updateKycReducer';
 import { updateSong } from './updateSongReducer';
 
