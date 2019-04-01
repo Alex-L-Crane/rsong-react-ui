4f6b3510538bef657676a961bf103023039cb8b0
Stefan Spasic <stefan.spasic@cramick-it.com>
2019-04-01 13:54:29 +0200

    kyc form, image upload

--- src/components/form-inputs/Radio.js
+++ src/components/form-inputs/Radio.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 
 class Checkbox extends Component {
-  render() {
-    return (
-      <span className="checkbox-container ph0">
-        <input
-          type="radio"
-          name={this.props.name}
-          id={this.props.id}
-          value={this.props.id}
-          className="hidebbutton"/>
-        <label htmlFor={this.props.id} className="checkmark dib w2 h2 br1 ba tc f5 pointer v-mid"></label>
-      </span>
-    );
-  }
+	render() {
+		return (
+			<span className="checkbox-container ph0">
+				<input
+					type="radio"
+					name={this.props.name}
+					id={this.props.id}
+					value={this.props.value}
+					className="hidebbutton"
+					onChange={this.props.onChange}
+				/>
+				<label htmlFor={this.props.id} className="checkmark dib w2 h2 br1 ba tc f5 pointer v-mid"></label>
+			</span>
+		);
+	}
 }
 
 export default Checkbox;
