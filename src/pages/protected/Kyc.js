1e2e775a34c9177d8b0361ed2f4f30f369429f97
Stefan Spasic <stefan.spasic@cramick-it.com>
2019-04-03 14:45:38 +0200

    ErrorMessages component

--- src/pages/protected/Kyc.js
+++ src/pages/protected/Kyc.js
@@ -14,6 +14,7 @@ import Terms from '../../components/notifications/Terms';
 import { validateKycForm } from '../../validators/kycValidator';
 import { getCountries } from '../../redux/actions/countriesActions';
 import BasicDatePicker from '../../components/form-inputs/BasicDatePicker';
+import ErrorMessages from '../../components/notifications/ErrorMessages';
 
 class Kyc extends Component {
 	constructor(props) {
@@ -314,7 +315,9 @@ class Kyc extends Component {
 
 						{Object.keys(this.state.errors).length > 0 && this.state.errors.constructor === Object ? 
 							(
-								<span>* Fill out required fields before proceeding</span>
+								<ErrorMessages
+									errorMessages={['* Fill out required fields before proceeding']}
+								/>
 							) : (
 								<></>
 							)
