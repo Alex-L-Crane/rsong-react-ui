89890363b5587ec19a507fe7793517afccda0309
Stefan Spasic <stefan.spasic@cramick-it.com>
2019-04-10 09:14:38 +0200

    reducer fix

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import { countries } from './countriesReducer';
-import { errorModal } from './errorModalReducer';
 import { genres } from './genresReducer';
 import { kyc } from './kycReducer';
 import { kycStatus } from './kycStatusReducer';
@@ -9,7 +8,6 @@ import { songs } from './songsReducer';
 
 export const rootReducer = combineReducers({
     countries,
-    errorModal,
     genres,
     kyc,
     kycStatus,
