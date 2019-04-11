b6c7a0d19cda6d28c95f0517112c81093da23e72
Alex Crane <alexcranedesigner@gmail.com>
2019-04-10 22:42:43 -0700

    added text only component for displaying uploaded songs without album art

--- src/pages/protected/Songs.js
+++ src/pages/protected/Songs.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import AppHeader from '../../components/header/AppHeader';
 import AddMediaTile from '../../components/mediaTile/AddMediaTile'
-import MediaTile from '../../components/mediaTile/MediaTile';
+import MediaRow from '../../components/mediaTile/MediaRow';
 import { getSongs } from '../../redux/actions/songsActions';
 
 class Songs extends Component {
@@ -23,16 +23,17 @@ class Songs extends Component {
 					pageTitle="Add or edit songs"
 				/>
 				<section className="ph5 pv2">
-					<div className="pt5 flex flex-wrap">
+					<div className="pt5 flex">
 						<AddMediaTile caption="Add new song" onAddMedia={this.onAddMedia}/>
-						{Array.isArray(this.props.songs) ? this.props.songs.map((song) => 
-							<MediaTile 
-								key={song._id}
-								name={song.title}
-								status={song.state}
-								image={song.image}
-							/>
-						) : <></>}
+						<div className="flex-grow">
+								{Array.isArray(this.props.songs) ? this.props.songs.map((song) =>
+									<MediaRow
+										key={song._id}
+										name={song.title}
+										status={song.state}
+										image={song.image}/>
+								) : <></>}
+						</div>
 					</div>
 				</section>
 			</section>
