cdfe2be6317f428a58364a94b8d41e42b033792a
Alex Crane <alexcranedesigner@gmail.com>
2019-04-16 00:47:51 -0700

    started creating account creation pages

--- src/pages/public/PublicPagesContainer.js
+++ src/pages/public/PublicPagesContainer.js
@@ -3,36 +3,46 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import Login from './Login';
 import FacebookEmail from './FacebookEmail';
 
+import AccountEmail from './createaccount/AccountEmail'
+
 class PublicPagesContainer extends Component {
     render() {
         return (
             <Switch>
                 <Route
                     exact
-                    path="/login" 
+                    path="/login"
                     render={(props) => (
-                        localStorage.getItem('login') ? 
-                           <Redirect to={{ pathname: '/' }} />  : <Login {...props} />  
+                        localStorage.getItem('login') ?
+                           <Redirect to={{ pathname: '/' }} />  : <Login {...props} />
                     )}
                 />
                 <Route
                     exact
-                    path="/login/email" 
+                    path="/login/signup-email"
                     render={(props) => (
-                        localStorage.getItem('login') ? 
-                           <Redirect to={{ pathname: '/' }} />  : <FacebookEmail {...props} />  
+                        localStorage.getItem('login') ?
+                           <Redirect to={{ pathname: '/' }} />  : <AccountEmail {...props} />
                     )}
                 />
                 <Route
-                    path="/*"                
+                    exact
+                    path="/login/email"
                     render={(props) => (
-                        !localStorage.getItem('login') ?       
+                        localStorage.getItem('login') ?
+                           <Redirect to={{ pathname: '/' }} />  : <FacebookEmail {...props} />
+                    )}
+                />
+                <Route
+                    path="/*"
+                    render={(props) => (
+                        !localStorage.getItem('login') ?
                             <Redirect to={{ pathname: '/', state: { from: props.location } }} /> : <></>
                     )}
                 />
             </Switch>
         );
     }
-} 
+}
 
 export default PublicPagesContainer;
